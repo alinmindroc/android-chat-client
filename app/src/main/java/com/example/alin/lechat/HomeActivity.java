4f9943709f9cd762d1a69ab59eff91dfc3f330dd
Alin Mindroc <mindroc.alin@gmail.com>
2016-04-03 14:48:09 +0300

    use view type in array adapter to show last item in friend and group lists differently

--- app/src/main/java/com/example/alin/lechat/HomeActivity.java
+++ app/src/main/java/com/example/alin/lechat/HomeActivity.java
@@ -1,20 +1,23 @@
 package com.example.alin.lechat;
 
 import android.content.Context;
-import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private final String ADD_FRIEND_LIST_ENTRY = "+ Add Friend";
+    private final String ADD_GROUP_LIST_ENTRY = "+ Add Group";
+    private final String ADD_COLOR_HEX = "#2C90D8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +43,6 @@ public class HomeActivity extends AppCompatActivity {
         friendList.setAdapter(friendsAdapter);
 
 
-
-
         //add groups
         ArrayList<Group> arrayOfGroups = new ArrayList<Group>();
         // Create the adapter to convert the array to views
@@ -58,7 +59,12 @@ public class HomeActivity extends AppCompatActivity {
         groupList.setAdapter(groupsAdapter);
 
 
+        //add groups and friends
+        arrayOfUsers.add(new User(ADD_FRIEND_LIST_ENTRY));
+        arrayOfGroups.add(new Group(ADD_GROUP_LIST_ENTRY));
 
+
+        //add logic for opening conversations
     }
 
     class User {
@@ -70,23 +76,44 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     class UsersAdapter extends ArrayAdapter<User> {
+        int dataSize;
+        private final int LAST_POSITION_VIEW_TYPE = 1;
+        private final int NON_LAST_POSITION_VIEW_TYPE = 0;
+
         public UsersAdapter(Context context, ArrayList<User> users) {
             super(context, 0, users);
+            this.dataSize = users.size();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            // Define a way to determine which layout to use, here it's just evens and odds.
+            return position == dataSize ? LAST_POSITION_VIEW_TYPE : NON_LAST_POSITION_VIEW_TYPE;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 2; // Count of different layouts
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            // Get the data item for this position
             User user = getItem(position);
-            // Check if an existing view is being reused, otherwise inflate the view
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.friend_list_entry, parent, false);
+
+            LayoutInflater lInflater = LayoutInflater.from(getContext());
+            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+                convertView = lInflater.inflate(R.layout.add_list_entry, parent, false);
+            } else {
+                convertView = lInflater.inflate(R.layout.friend_list_entry, parent, false);
             }
-            // Lookup view for data population
+
             TextView tvName = (TextView) convertView.findViewById(R.id.nameLabel);
-            // Populate the data into the template view using the data object
             tvName.setText(user.name);
-            // Return the completed view to render on screen
+
+            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+                tvName.setTextColor(Color.parseColor(ADD_COLOR_HEX));
+            }
+
             return convertView;
         }
     }
@@ -100,25 +127,45 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     class GroupsAdapter extends ArrayAdapter<Group> {
+        int dataSize;
+        private final int LAST_POSITION_VIEW_TYPE = 1;
+        private final int NON_LAST_POSITION_VIEW_TYPE = 0;
+
         public GroupsAdapter(Context context, ArrayList<Group> groups) {
             super(context, 0, groups);
+            this.dataSize = groups.size();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            // Define a way to determine which layout to use, here it's just evens and odds.
+            return position == dataSize ? LAST_POSITION_VIEW_TYPE : NON_LAST_POSITION_VIEW_TYPE;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 2; // Count of different layouts
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            // Get the data item for this position
             Group group = getItem(position);
-            // Check if an existing view is being reused, otherwise inflate the view
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.group_list_entry, parent, false);
+
+            LayoutInflater lInflater = LayoutInflater.from(getContext());
+            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+                convertView = lInflater.inflate(R.layout.add_list_entry, parent, false);
+            } else {
+                convertView = lInflater.inflate(R.layout.group_list_entry, parent, false);
             }
-            // Lookup view for data population
+
             TextView tvName = (TextView) convertView.findViewById(R.id.nameLabel);
-            // Populate the data into the template view using the data object
             tvName.setText(group.name);
-            // Return the completed view to render on screen
+
+            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+                tvName.setTextColor(Color.parseColor(ADD_COLOR_HEX));
+            }
+
             return convertView;
         }
     }
-
 }
