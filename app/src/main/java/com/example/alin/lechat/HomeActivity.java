c1ef1f0bff8832c8afc04210c873e53ced7fcee8
Alin Mindroc <mindroc.alin@gmail.com>
2016-04-03 15:11:28 +0300

    move add list items at first position

--- app/src/main/java/com/example/alin/lechat/HomeActivity.java
+++ app/src/main/java/com/example/alin/lechat/HomeActivity.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -29,7 +31,7 @@ public class HomeActivity extends AppCompatActivity {
 
         //add users
 
-        ArrayList<User> arrayOfUsers = new ArrayList<User>();
+        final ArrayList<User> arrayOfUsers = new ArrayList<User>();
         // Create the adapter to convert the array to views
 
         arrayOfUsers.add(new User("Alin"));
@@ -60,11 +62,32 @@ public class HomeActivity extends AppCompatActivity {
 
 
         //add groups and friends
-        arrayOfUsers.add(new User(ADD_FRIEND_LIST_ENTRY));
-        arrayOfGroups.add(new Group(ADD_GROUP_LIST_ENTRY));
+        arrayOfUsers.add(0, new User(ADD_FRIEND_LIST_ENTRY));
+        arrayOfGroups.add(0, new Group(ADD_GROUP_LIST_ENTRY));
 
 
         //add logic for opening conversations
+        friendList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    //start add friend activity
+                } else {
+                    //go to friend conversation
+                }
+            }
+        });
+
+        groupList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    //start add group activity
+                } else {
+                    //go to group conversation
+                }
+            }
+        });
     }
 
     class User {
@@ -77,8 +100,8 @@ public class HomeActivity extends AppCompatActivity {
 
     class UsersAdapter extends ArrayAdapter<User> {
         int dataSize;
-        private final int LAST_POSITION_VIEW_TYPE = 1;
-        private final int NON_LAST_POSITION_VIEW_TYPE = 0;
+        private final int FIRST_POSITION_VIEW_TYPE = 1;
+        private final int NON_FIRST_POSITION_VIEW_TYPE = 0;
 
         public UsersAdapter(Context context, ArrayList<User> users) {
             super(context, 0, users);
@@ -88,7 +111,7 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public int getItemViewType(int position) {
             // Define a way to determine which layout to use, here it's just evens and odds.
-            return position == dataSize ? LAST_POSITION_VIEW_TYPE : NON_LAST_POSITION_VIEW_TYPE;
+            return position == 0 ? FIRST_POSITION_VIEW_TYPE : NON_FIRST_POSITION_VIEW_TYPE;
         }
 
         @Override
@@ -101,7 +124,7 @@ public class HomeActivity extends AppCompatActivity {
             User user = getItem(position);
 
             LayoutInflater lInflater = LayoutInflater.from(getContext());
-            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+            if (getItemViewType(position) == FIRST_POSITION_VIEW_TYPE) {
                 convertView = lInflater.inflate(R.layout.add_list_entry, parent, false);
             } else {
                 convertView = lInflater.inflate(R.layout.friend_list_entry, parent, false);
@@ -110,7 +133,7 @@ public class HomeActivity extends AppCompatActivity {
             TextView tvName = (TextView) convertView.findViewById(R.id.nameLabel);
             tvName.setText(user.name);
 
-            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+            if (getItemViewType(position) == FIRST_POSITION_VIEW_TYPE) {
                 tvName.setTextColor(Color.parseColor(ADD_COLOR_HEX));
             }
 
@@ -128,8 +151,8 @@ public class HomeActivity extends AppCompatActivity {
 
     class GroupsAdapter extends ArrayAdapter<Group> {
         int dataSize;
-        private final int LAST_POSITION_VIEW_TYPE = 1;
-        private final int NON_LAST_POSITION_VIEW_TYPE = 0;
+        private final int FIRST_POSITION_VIEW_TYPE = 1;
+        private final int NON_FIRST_POSITION_VIEW_TYPE = 0;
 
         public GroupsAdapter(Context context, ArrayList<Group> groups) {
             super(context, 0, groups);
@@ -139,7 +162,7 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public int getItemViewType(int position) {
             // Define a way to determine which layout to use, here it's just evens and odds.
-            return position == dataSize ? LAST_POSITION_VIEW_TYPE : NON_LAST_POSITION_VIEW_TYPE;
+            return position == 0 ? FIRST_POSITION_VIEW_TYPE : NON_FIRST_POSITION_VIEW_TYPE;
         }
 
         @Override
@@ -152,7 +175,7 @@ public class HomeActivity extends AppCompatActivity {
             Group group = getItem(position);
 
             LayoutInflater lInflater = LayoutInflater.from(getContext());
-            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+            if (getItemViewType(position) == FIRST_POSITION_VIEW_TYPE) {
                 convertView = lInflater.inflate(R.layout.add_list_entry, parent, false);
             } else {
                 convertView = lInflater.inflate(R.layout.group_list_entry, parent, false);
@@ -161,7 +184,7 @@ public class HomeActivity extends AppCompatActivity {
             TextView tvName = (TextView) convertView.findViewById(R.id.nameLabel);
             tvName.setText(group.name);
 
-            if (getItemViewType(position) == LAST_POSITION_VIEW_TYPE) {
+            if (getItemViewType(position) == FIRST_POSITION_VIEW_TYPE) {
                 tvName.setTextColor(Color.parseColor(ADD_COLOR_HEX));
             }
 
